ca6d3eb8bed1c719930635a63003c44ad1870708
sulthann1 <sulthanaawaliya155@gmail.com>
2026-03-13 22:51:18 +0700

    menggunakakn execute non query untuk update data

--- Form1.cs
+++ Form1.cs
@@ -82,5 +82,23 @@ namespace praktikum4
                 MessageBox.Show(ex.Message);
             }
         }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "UPDATE Mahasiswa SET Alamat = 'Yogyakarta' WHERE NIM='23110100001'";
+                cmd = new SqlCommand(query, conn);
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh = " + hasil);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
