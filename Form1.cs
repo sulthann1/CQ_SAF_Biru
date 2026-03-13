e294760d3d1e4631ce14b0259ce13990c732ce3d
sulthann1 <sulthanaawaliya155@gmail.com>
2026-03-13 22:45:13 +0700

    membuka koneksi database

--- Form1.cs
+++ Form1.cs
@@ -31,5 +31,20 @@ namespace praktikum4
         {
 
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
