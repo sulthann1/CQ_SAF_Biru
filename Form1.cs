85e0ad8b754d9cf045090301012e0669e9ed1cb5
sulthann1 <sulthanaawaliya155@gmail.com>
2026-03-13 22:42:38 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace praktikum4
+{
+    public partial class Form1: Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+
+        private void Koneksi()
+        {
+            conn = new SqlConnection(
+                "Data Source=SULTHANN\\SULTHANSERVER; Initial Catalog=DBAkademikADO; Integrated Security=True"
+                );
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
