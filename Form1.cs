2f81876f5b08414a1b71ab7629837f0175e37b47
sulthann1 <sulthanaawaliya155@gmail.com>
2026-03-13 23:16:37 +0700

    merevisi nama class tiap button

--- Form1.cs
+++ Form1.cs
@@ -101,7 +101,7 @@ namespace praktikum4
             }
         }
 
-        private void button5_Click(object sender, EventArgs e)    
+        private void buttonHitungDs_Click(object sender, EventArgs e)    
         {
             try
             {
@@ -119,9 +119,22 @@ namespace praktikum4
             }
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void buttonUpdateMk_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "UPDATE MataKuliah SET SKS = 4 WHERE KodeMK = 'IF210101'";
+                cmd = new SqlCommand(query, conn);
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh = " + hasil);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
