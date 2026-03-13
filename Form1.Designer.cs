2be7d509dc8bf7fab0cf81a00e509dfce516fd29
sulthann1 <sulthanaawaliya155@gmail.com>
2026-03-13 23:26:06 +0700

    koneksi button di properties

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -123,7 +123,7 @@
             this.button7.TabIndex = 8;
             this.button7.Text = "Input Pr";
             this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.Click += new System.EventHandler(this.buttonInputPr_Click);
             // 
             // Form1
             // 
